8b02b158b66a23a46eefb656beb8387b1c945d53
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-12-28 16:33:58 +0100

    doc

--- example/src/lib/WSCanvasProps.tsx
+++ example/src/lib/WSCanvasProps.tsx
@@ -15,173 +15,173 @@ export interface WSCanvasCellDataNfo {
 
 /** see WSCanvasPropsDefault for default values */
 export interface WSCanvasProps {
-    /** width 100% */
+    /** width 100% [default: false] */
     fullwidth: boolean;
-    /** width of canvas */
+    /** width of canvas [default: window.innerWidth] */
     width: number;
-    /** height of canvas */
+    /** height of canvas [default: window.innerHeight] */
     height: number;
-    /** datasource to sync refresh */
+    /** datasource to sync refresh [default: null] */
     dataSource: any;
-    /** nr of rows in the grid */
+    /** nr of rows in the grid [default: 0] */
     rowsCount: number;
-    /** nr of cols in the grid ( or use columns ) */
+    /** nr of cols in the grid ( or use columns ) [default: 0] */
     colsCount?: number;
-    /** compact column info */
+    /** compact column info [default: undefined] */
     columns?: WSCanvasColumn[];
-    /** width of column in the grid ( or use columns ) */
+    /** width of column in the grid ( or use columns ) [default: undefined] */
     colWidth?: (cidx: number) => number;
-    /** expand column width to fit control width ( if column width sum not already exceed control width ) */
+    /** expand column width to fit control width ( if column width sum not already exceed control width ) [default: true] */
     colWidthExpand: boolean;
-    /** height of rows in the grid */
+    /** height of rows in the grid [default: DEFAULT_ROW_HEIGHT=30] */
     rowHeight: (ridx: number) => number;
-    /** nr of frozen rows */
+    /** nr of frozen rows [default: 0] */
     frozenRowsCount: number;
-    /** nr of frozen cols */
+    /** nr of frozen cols [default: 0] */
     frozenColsCount: number;
-    /** selection mode allow append using ctrl key */
+    /** selection mode allow append using ctrl key [default: true] */
     selectionModeMulti: boolean;
-    /** selection mode row or cell */
-    selectionMode: WSCanvasSelectMode;    
-    /** show focused cell outline */
+    /** selection mode row or cell [default: WSCanvasSelectMode.Cell] */
+    selectionMode: WSCanvasSelectMode;
+    /** show focused cell outline [default: false] */
     showFocusedCellOutline: boolean;
-    /** show row numbers column */
+    /** show row numbers column [default: false] */
     showRowNumber: boolean;
-    /** highlight row header matching current sel */
+    /** highlight row header matching current sel [default: true] */
     highlightRowNumber: boolean;
-    /** show column headers row */
+    /** show column headers row [default: false] */
     showColNumber: boolean;
-    /** highlight column header matching current sel */
+    /** highlight column header matching current sel [default: true] */
     highlightColNumber: boolean;
-    /** behavior for column header click sort or select */
+    /** behavior for column header click sort or select [default: WSCanvasColumnClickBehavior.ToggleSort] */
     columnClickBehavior: WSCanvasColumnClickBehavior;
-    /** show column filters row */
+    /** show column filters row [default: false] */
     showFilter: boolean;
-    /** whatever select first avail row after filtering */
+    /** whatever select first avail row after filtering [default: false] */
     selectFirstOnFilter: boolean;
-    /** show or truncate partial columns */
+    /** show or truncate partial columns [default: true] */
     showPartialColumns: boolean;
-    /** show or truncate partial rows */
+    /** show or truncate partial rows [default: true] */
     showPartialRows: boolean;
-    /** prevent wheel default window scroll when scroll at top or bottom */
+    /** prevent wheel default window scroll when scroll at top or bottom [default: true] */
     preventWheelOnBounds: boolean;
-    /** if set new rows goes inserted at given view index  */
-    newRowsInsertAtViewIndex?: number;    
+    /** if set new rows goes inserted at given view index [default: undefined] */
+    newRowsInsertAtViewIndex?: number;
 
-    /** retrieve data from a cell ( or use columns ) */
+    /** retrieve data from a cell ( or use columns ) [default: undefined] */
     getCellData?: (coord: WSCanvasCellCoord) => any;
-    /** allow to transform data before being displayed (useful for enum types); if defined must return input data as is or transformed ( or use columns ) */
+    /** allow to transform data before being displayed (useful for enum types); if defined must return input data as is or transformed ( or use columns ) [default: undefined] */
     renderTransform?: (cell: WSCanvasCellCoord, data: any) => any;
-    /** retrieve cells dataset copy */
+    /** retrieve cells dataset copy [default: null] */
     prepareCellDataset: () => any;
-    /** apply change to dataset */
+    /** apply change to dataset [default: {}] */
     setCellData: (dataset: any, cell: WSCanvasCellCoord, value: any) => void;
-    /** set cell dataset state */
+    /** set cell dataset state [default: {}] */
     commitCellDataset: (dataset: any) => void;
-    /** allow to define a custom editor or return undefined to use builtin cell editor */
+    /** allow to define a custom editor or return undefined to use builtin cell editor [default: undefined] */
     getCellCustomEdit?: ((states: WSCanvasStates, cell: WSCanvasCellCoord,
         containerStyle?: CSSProperties, cellWidth?: number, cellHeight?: number) => JSX.Element) | undefined;
-    /** header of given col ( or use columns ) */
+    /** header of given col ( or use columns ) [default: undefined] */
     getColumnHeader?: (col: number) => string | undefined;
-    /** states whatever colum should hidden */
-    getColumnHidden?: (col:number) => boolean;
-    /** column sort method  ( or use columns ) */
+    /** states whatever colum should hidden [default: undefined] */
+    getColumnHidden?: (col: number) => boolean;
+    /** column sort method  ( or use columns ) [default: undefined] */
     getColumnLessThanOp?: (col: number) => ((a: any, b: any) => boolean) | undefined;
-    /** specify type of a cell ( or use columns)  */
+    /** specify type of a cell ( or use columns)  [default: undefined] */
     getCellType?: (coord: WSCanvasCellCoord, value: any) => WSCanvasColumnType | undefined;
-    /** specify cell editor inhibit ( or use columns ) */
+    /** specify cell editor inhibit ( or use columns ) [default: undefined] */
     isCellReadonly?: (coord: WSCanvasCellCoord) => boolean | undefined;
-    /** specify predefined column sort ( WSCanvasColumn array helper or use columns ) */
+    /** specify predefined column sort ( WSCanvasColumn array helper or use columns ) [default: undefined] */
     columnInitialSort?: WSCanvasColumnSortInfo[] | undefined;
-    /** specify text align of a cell ( or use columns ) */
+    /** specify text align of a cell ( or use columns ) [default: undefined] */
     getCellTextAlign?: (coord: WSCanvasCellCoord, value: any) => CanvasTextAlign | undefined;
 
-    /** individual cell background customization */
+    /** individual cell background customization [default: undefined] */
     getCellBackgroundColor?: (coord: WSCanvasCellCoord, props: WSCanvasProps) => string | undefined;
-    /** cell background */
+    /** cell background [default: "white"] */
     sheetBackgroundColor: string;
-    /** grid lines color */
+    /** grid lines color [default: "#c0c0c0"] */
     gridLinesColor: string;
-    /** color of frozen row/cols separator line*/
+    /** color of frozen row/cols separator line [default: "black"] */
     frozenCellGridLinesColor: string;
-    /** color or focused cell outline */
+    /** color or focused cell outline [default: "black"] */
     focusedCellBorderColor: string;
 
-    /** background color of selected cells */
+    /** background color of selected cells [default: "#f9d4c7"] */
     selectionBackgroundColor: string;
-    /** border color of selection */
+    /** border color of selection [default: "#e95420"] */
     selectionBorderColor: string;
-    /** color of row/col numbers */
+    /** color of row/col numbers [default: "white"] */
     selectedHeaderTextColor: string;
-    /** background of row/col numbers */
+    /** background of row/col numbers [default: "#e95420"] */
     selectedHeaderBackgroundColor: string;
 
-    /** moment formatting for date type cells */
+    /** moment formatting for date type cells [default: "L"] */
     dateCellMomentFormat: string;
-    /** moment formatting for time type cells */
+    /** moment formatting for time type cells [default: "LT"] */
     timeCellMomentFormat: string;
-    /** moment formatting for datetime type cells */
+    /** moment formatting for datetime type cells (ex. "L LTS" to include seconds) [default: "L LT"] */
     dateTimeCellMomentFormat: string;
-    /** margin of text inside cells */
+    /** margin of text inside cells [default: 2] */
     textMargin: number;
-    /** individual cell text wrap ( or use columns ) */
+    /** individual cell text wrap ( or use columns ) [default: undefined] */
     getCellTextWrap?: (coord: WSCanvasCellCoord, props: WSCanvasProps) => boolean | undefined;
-    /** individual cell font customization */
+    /** individual cell font customization [default: undefined] */
     getCellFont?: (coord: WSCanvasCellCoord, props: WSCanvasProps) => string | undefined;
-    /** font of cells text */
+    /** font of cells text [default: "12px Liberation Sans"] */
     font: string;
-    /** individual cell text color customization */
+    /** individual cell text color customization [default: undefined] */
     getCellTextColor?: (coord: WSCanvasCellCoord, props: WSCanvasProps) => string | undefined;
-    /** color of cell text */
+    /** color of cell text [default: "black"] */
     cellTextColor: string;
-    /** font of row/col nunbers */
+    /** font of row/col nunbers [default: "16px Liberation Sans"] */
     headerFont: string;
-    /** default cell cursor */
+    /** default cell cursor [default: "default"] */
     cellCursor: string;
-    /** default non cell cursor */
+    /** default non cell cursor [default: "default"] */
     outsideCellCursor: string;
-    /** row hover */
+    /** row hover (ex. "rgba(250,250,250,0.5)") [default: undefined] */
     rowHoverColor: string | undefined;
 
-    /** filter apply debounce (ms) */
+    /** filter apply debounce (ms) [default: 500] */
     filterDebounceMs: number;
-    /** filter edit cell margin */
+    /** filter edit cell margin [default: 3] */
     filterTextMargin: number;
-    /** filter match ignore case */
+    /** filter match ignore case [default: true] */
     filterIgnoreCase: boolean;
-    /** filter cell background color */
-    filterBackground: string;    
+    /** filter cell background color [default: "yellow"] */
+    filterBackground: string;
 
-    /** ms from last column change to recompute row height */
+    /** ms from last column change to recompute row height [default: 0] */
     recomputeRowHeightDebounceFilterMs: number;
-    /** width of row numbers col */
+    /** width of row numbers col [default: 80] */
     rowNumberColWidth: number;
-    /** height of column numbers row */
+    /** height of column numbers row [default: DEFAULT_ROW_HEIGHT=30] */
     colNumberRowHeight: number;
-    /** background of row/col number cells */
+    /** background of row/col number cells [default: "#f5f6f7"] */
     cellNumberBackgroundColor: string;
 
-    /** mode of vertical scrollbar ( auto, on, off ) */
+    /** mode of vertical scrollbar ( auto, on, off ) [default: WSCanvasScrollbarMode.auto] */
     verticalScrollbarMode: WSCanvasScrollbarMode;
-    /** mode of horizontal scrollbar ( auto, on, off ) */
+    /** mode of horizontal scrollbar ( auto, on, off ) [default: WSCanvasScrollbarMode.auto] */
     horizontalScrollbarMode: WSCanvasScrollbarMode;
-    /** thickness of scrollbar */
+    /** thickness of scrollbar [default: 10] */
     scrollBarThk: number;
-    /** min length of scrollbar handle */
+    /** min length of scrollbar handle [default: 20] */
     minScrollHandleLen: number;
-    /** scrollbar handle color */
+    /** scrollbar handle color [default: "#878787"] */
     scrollBarColor: string;
-    /** scrollbar handle actived color */
+    /** scrollbar handle actived color [default: "red"] */
     clickedScrollBarColor: string;
 
-    /** div container custom styles */
+    /** div container custom styles [default: undefined] */
     containerStyle: CSSProperties | undefined;
-    /** canvas custom styles ( margin and padding will overriden to 0; use containerStyle for these ) */
+    /** canvas custom styles ( margin and padding will overriden to 0; use containerStyle for these ) [default: undefined] */
     canvasStyle: CSSProperties | undefined;
 
-    /** enable debug div ( for dev purpose ) */
+    /** enable debug div ( for dev purpose ) [default: false] */
     debug: boolean;
-    /** div where to place debug */
+    /** div where to place debug [default: undefined] */
     dbgDiv: React.RefObject<HTMLDivElement> | undefined;
 
     /** receive api */
